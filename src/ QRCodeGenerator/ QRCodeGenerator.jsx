fc4e4a379f35583bc48bf9228c9f363949ce5205
Md Fuad Amir <mdfuadamir@gmail.com>
2025-12-28 00:05:28 +0600

    added complete qr genarator

--- src/ QRCodeGenerator/ QRCodeGenerator.jsx
+++ src/ QRCodeGenerator/ QRCodeGenerator.jsx
@@ -1,11 +1,85 @@
+import logo from "../assets/qrify.png";
+import { FaDownload, FaQrcode } from "react-icons/fa";
+import { QRCodeCanvas } from "qrcode.react";
+import { useState } from "react";
 
+const QRCodeGenerator = () => {
+  const [text, setText] = useState("");
+  const [bgColor, setBgColor] = useState();
+  const [fgColor, setFgColor] = useState();
 
-const  QRCodeGenerator = () => {
-    return (
-        <div>
-            
+  const downloadQR = () => {};
+  return (
+    <div className=" max-w-md w-full">
+      <div className="bg-white/20 p-4 md:p-6 rounded-2xl">
+        {/* heading */}
+        <h1 className="text-white text-2xl font-bold font-serif flex justify-center items-center">
+          <img src={logo} alt="/qrify" className="w-12" /> QR Code Genarator
+        </h1>
+        <p className="px-8 text-gray-200 text-center">
+          type your text,image url, pdf url & get the QR code
+        </p>
+        {/* input */}
+        <input
+          type="text"
+          placeholder="Enter tent or URL..."
+          value={text}
+          onChange={(e) => setText(e.target.value)}
+          className="w-full px-4 py-3 border rounded-lg my-4 outline-none focus:ring-2 focus:ring-green-400 border-white text-white placeholder-gray-300"
+        />
+        {/* CONTROLER */}
+        <div className="grid grid-cols-2 gap-4 mb-4 text-sm">
+          {/* qr color */}
+          <div>
+            <label className="block mb-1 text-white">QR color</label>
+            <input
+              type="color"
+              value={fgColor}
+              onChange={(e) => setFgColor(e.target.value)}
+              className="w-full h-10"
+            />
+          </div>
+          {/* qr bg color */}
+          <div>
+            <label className="block mb-1 text-white">Background</label>
+            <input
+              type="color"
+              value={fgColor}
+              onChange={(e) => setBgColor(e.target.value)}
+              className="w-full h-10"
+            />
+          </div>
         </div>
-    );
+        {/* qr preview */}
+        <div className="flex justify-center my-8">
+          {text ? (
+            <QRCodeCanvas
+              id="qr-code"
+              value={text}
+              bgColor={bgColor}
+              fgColor={fgColor}
+              level="H"
+              includeMargin
+            />
+          ) : (
+            <div className="flex flex-col justify-center  items-center">
+              <FaQrcode className="text-4xl text-green-100 mb-2" />
+              <p className="text-gray-200 text-sm">Enter text to generate QR</p>
+            </div>
+          )}
+        </div>
+        {/* downloade */}
+
+        <button
+          onClick={downloadQR}
+          disabled={!text}
+          className="w-full bg-blue-500 text-white py-3 rounded-lg font-semibold disabled:opacity-50 flex justify-center items-center gap-4"
+        >
+          <FaDownload /> Doqnloade QR code
+        </button>
+      </div>
+    </div>
+  );
 };
 
-export default  QRCodeGenerator;
+export default QRCodeGenerator;
--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import { Toaster } from "react-hot-toast";
 import QRCodeGenerator from "./ QRCodeGenerator/ QRCodeGenerator";
-import logo from "../src/assets/qrify.png"
+import logo from "../src/assets/qrify.png";
 const App = () => {
   return (
     <div className="relative min-h-screen overflow-hidden">
@@ -11,19 +11,46 @@ const App = () => {
           backgroundImage:
             "url(https://i.ibb.co.com/XZ62XpdQ/qr-code-table-1.jpg)",
         }}
-      >
+      > 
         <div className="absolute inset-0 bg-black/50 backdrop-blur-sm" />
       </div>
       {/* Content */}
-      <div className="relative z-10 flex items-center justify-center min-h-screen px-4 py-8">
-        <QRCodeGenerator />
+      <div className="relative z-10 flex flex-col md:flex-row  justify-center items-center  min-h-screen px-4 md:px-12  py-8 gap-6">
+        <div className="flex-1 flex justify-center">
+          <QRCodeGenerator />
+        </div>
+        <div className="flex-1">
+          <img src={logo} alt="" className="w-44 mx-auto"/>
+        <p className="text-gray-200 text-center">
+          <b className="text-green-500">QRIFY </b>Web is a modern and user-friendly web application designed to
+          generate QR codes quickly and efficiently. It allows users to convert
+          text, links, images, and PDF files into scannable QR codes with just a
+          few clicks. The goal of QRify Web is to make sharing digital content
+          simple, fast, and accessible for everyone. The application features a
+          clean and responsive interface that works smoothly on both desktop and
+          mobile devices. Users can enter any text or URL and instantly see a
+          live QR code preview. For advanced usage, QRify Web supports uploading
+          images and PDF files, generating QR codes that link directly to those
+          files. QRify Web uses modern web technologies to ensure high
+          performance and reliability. The QR codes generated are high quality
+          and can be downloaded as PNG images. This makes them perfect for
+          printing, sharing on social media, or embedding into documents.
+          Security and simplicity are core principles of the platform. No
+          unnecessary data is stored, and the user experience remains fast and
+          lightweight. The application is designed for students, developers,
+          business owners, and anyone who needs quick QR solutions. QRify Web is
+          ideal for sharing documents, promotional materials, websites, or
+          digital resources.
+        </p>
+        </div>
       </div>
-      <div className="relative flex justify-center items-center">
-        <img src={logo} alt="/qrify" className="w-8 h-8"/>
-        <p className="text-white text-center p-4">Copyright © {new Date().getFullYear()} - Md Fuad Amir All right reserved</p>
-        <img src={logo} alt="/qrify" className="w-8 h-8"/>
+      <div className="relative">
+        <p className="text-white text-center p-4">
+          Copyright © {new Date().getFullYear()} - Md Fuad Amir All right
+          reserved
+        </p>
       </div>
-       <Toaster />
+      <Toaster />
     </div>
   );
 };
